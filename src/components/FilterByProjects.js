397042c428c154d2dc404a1b94ccb978dad67f14
Antoniya Hristova <karma12451@gmail.com>
2023-12-24 10:26:18 +0200

    fix linting

--- src/components/FilterByProjects.js
+++ src/components/FilterByProjects.js
@@ -1,56 +1,62 @@
-import '../assets/css/DataTable.css';
-import React from 'react';
+import "../assets/css/DataTable.css";
+import React from "react";
 
 let flag = false;
 
-function FilterByProjects({ data, project }){
+function FilterByProjects({ data, project }) {
   const array = [];
 
-  const projectEmployees = Object.groupBy(data, ({projectId}) => projectId);
-  console.log({projectEmployees});
+  const projectEmployees = Object.groupBy(data, ({ projectId }) => projectId);
+  console.log({ projectEmployees });
   for (const key in projectEmployees) {
     if (key === project) {
-        flag = true;
+      flag = true;
       const arrayOfObjects = projectEmployees[key];
-      if (Array.isArray(arrayOfObjects) && arrayOfObjects.every(obj => typeof obj === 'object')) {
+      if (
+        Array.isArray(arrayOfObjects) &&
+        arrayOfObjects.every((obj) => typeof obj === "object")
+      ) {
         arrayOfObjects.forEach((object, index) => {
-                array.push(object);
+          array.push(object);
         });
+      }
     }
-     
-}
-} 
+  }
 
-if(flag===false){
-    return (<h3>No project with this Id found!</h3>);
+  if (flag === false) {
+    return <h3>No project with this Id found!</h3>;
+  } else {
+    return (
+      <div className="FirstTable">
+        <table>
+          <thead>
+            <tr>
+              <th>Employee Id</th>
+              <th>Project Id</th>
+              <th>Start Date</th>
+              <th>Last Day</th>
+            </tr>
+          </thead>
+          <tbody>
+            {array.map((row, index) => {
+              return (
+                <tr key={index}>
+                  <td>{row.employeeId}</td>
+                  <td>{row.projectId}</td>
+                  <td>{new Date(row.startDate).toLocaleDateString("bg-BG")}</td>
+                  <td>
+                    {row.endDate
+                      ? new Date(row.endDate).toLocaleDateString("bg-BG")
+                      : "Ongoing"}
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
-else {
-  return (
-    <div className="FirstTable">
-      <table>
-        <thead>
-          <tr>
-            <th>Employee Id</th>
-            <th>Project Id</th>
-            <th>Start Date</th>
-            <th>Last Day</th>
-          </tr>
-        </thead>
-        <tbody>
-        {array.map( (row, index) =>{
-                    return (
-                        <tr key={index}>
-                            <td>{row.employeeId}</td>
-                            <td>{row.projectId}</td>
-                            <td>{new Date(row.startDate).toLocaleDateString("bg-BG")}</td>
-                            <td>{row.endDate ? new Date(row.endDate).toLocaleDateString("bg-BG") : "Ongoing"}</td>
-                        </tr>
-                    )
-                } )}
-        </tbody>
-      </table>
-    </div>
-  );}
-};
 
 export default FilterByProjects;
--- src/components/FindWorkerPairs.js
+++ src/components/FindWorkerPairs.js
@@ -1,47 +1,46 @@
 import React from "react";
-import '../assets/css/Style.css';
-import Sorted from '../utils/Sorted.js'
+import "../assets/css/Style.css";
+import Sorted from "../utils/Sorted.js";
 
-function FinalTable(data){
+function FinalTable(data) {
+  const sorted = Sorted(data);
 
-const sorted = Sorted(data);
+  let max = 0;
 
-let max=0;
-
-for (const key in sorted) {
+  for (const key in sorted) {
     if (sorted.hasOwnProperty(key)) {
-
-      if(max < sorted[key].time){
+      if (max < sorted[key].time) {
         max = sorted[key].time;
-      }    
+      }
     }
   }
 
-  for(const key in sorted){
+  for (const key in sorted) {
     if (sorted.hasOwnProperty(key)) {
-        if(max === sorted[key].time){
-            const [id1, id2] = key.split("_");
+      if (max === sorted[key].time) {
+        const [id1, id2] = key.split("_");
 
-            return (
-                <div className="answer-div">
-                  <h2 >Pair with most days spent working together:</h2>
-                 <h3>Employee {id1} and employee {id2} worked as team the most!</h3>
-                  <ul className="answer">
-                    {sorted[key].proj.map((obj, index) => (
-                       <li key={index}>
-                      Project ID: {obj.projectId} - Days spent on project: {obj.timeForProject}
-                     </li>
-                    ))}
-                  </ul>
-                  <h2>Total days spent:</h2>
-                  <p>{sorted[key].time} days</p>
-                </div>
-              );
-        }    
+        return (
+          <div className="answer-div">
+            <h2>Pair with most days spent working together:</h2>
+            <h3>
+              Employee {id1} and employee {id2} worked as team the most!
+            </h3>
+            <ul className="answer">
+              {sorted[key].proj.map((obj, index) => (
+                <li key={index}>
+                  Project ID: {obj.projectId} - Days spent on project:{" "}
+                  {obj.timeForProject}
+                </li>
+              ))}
+            </ul>
+            <h2>Total days spent:</h2>
+            <p>{sorted[key].time} days</p>
+          </div>
+        );
       }
+    }
   }
-
 }
-
 
 export default FinalTable;
